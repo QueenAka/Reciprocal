fb697176961424eb25c00ae4c4a81d57b230ff5b
QueenAka <ghorneriv@gmail.com>
2025-04-22 19:14:40 -0400

    coolio fix

--- script.js
+++ script.js
@@ -224,6 +224,7 @@ function displayMessage(json) {
       holder.appendChild(div);
     });
     document.querySelector("main").appendChild(holder);
+    selectItem(1, 0);
     if (json.type == "ai") {
       const aud = new Audio("./media/send.wav");
       aud.play();
